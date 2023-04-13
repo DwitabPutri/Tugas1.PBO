5dfdf89bf2c13f5274f3d5ee9de4c309c63647ab
DwitabPutri <putrisuastini22@gmail.com>
2023-04-13 13:34:13 +0800

    Membuat class restoran dan uji coba commit kedua

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+class restoran{
+    String nama;
+    String alamat;
+    String noTelp;
+    String rating;
+
+    restoran(String nama, String alamat, String noTelp, String rating){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelp = noTelp;
+        this.rating = rating;
+    }
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Tes Commit Github");
