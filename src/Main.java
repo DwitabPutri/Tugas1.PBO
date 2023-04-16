33bfbf9d400634f35714570372e8095b282ee4a1
DwitabPutri <putrisuastini22@gmail.com>
2023-04-16 21:50:09 +0800

    Memperbarui kelas restoran

--- src/Main.java
+++ src/Main.java
@@ -30,95 +30,70 @@ class Utama {
     }
 }
 
-class Restoran {
-    String Nama;
-    float Rating;
-    String Alamat;
-    String noTelp;
-    String nmk1;
-    String nmk2;
-    String nmk3;
-    float hm1;
-    float hm2;
-    float hm3;
-    Restoran() {
+class Restoran{
+    ArrayList<Utama> makananResto = new ArrayList();
+    ArrayList<Utama> minumanResto = new ArrayList();
+    int Id;
+    String namaResto;
+    String alamatResto;
+
+    Restoran(){
     }
 
-    Restoran(String Nama, float Rating, String Alamat, String noTelp, String nmk1, String nmk2, String nmk3, float hm1, float hm2, float hm3) {
-        this.Nama = Nama;
-        this.Rating = Rating;
-        this.Alamat=Alamat;
-        this.noTelp=noTelp;
-        this.nmk1=nmk1;
-        this.nmk2=nmk2;
-        this.nmk3=nmk3;
-        this.hm1=hm1;
-        this.hm2=hm2;
-        this.hm3=hm3;
+    Restoran(int Id, String namaResto, String alamatResto){
+        this.Id = Id;
+        this.namaResto = namaResto;
+        this.alamatResto = alamatResto;
     }
 
-    void input() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Nama Restoran :  ");
-        this.Nama = input.nextLine();
-        System.out.print("Alamat Restoran : ");
-        this.Alamat=input.nextLine();
-        System.out.print("Nomor Telepon Restoran : ");
-        this.noTelp=input.nextLine();
-        System.out.print("Nama Makanan/Minuman Pertama : ");
-        this.nmk1=input.nextLine();
-        System.out.print("Nama Makanan/Minuman Kedua : ");
-        this.nmk2=input.nextLine();
-        System.out.print("Nama Makanan/Minuman Ketiga : ");
-        this.nmk3=input.nextLine();
-        System.out.print("Harga Makanan/Minuman Pertama : ");
-        this.hm1=input.nextFloat();
-        System.out.print("Harga Makanan/Minuman Kedua : ");
-        this.hm2=input.nextFloat();
-        System.out.print("Harga Makanan/Minuman Ketiga : ");
-        this.hm3=input.nextFloat();
+    int getId() {
+        return Id;
     }
 
-    String getNama() {
-        return this.Nama;
+    void setId(int Id) {
+        this.Id = Id;
     }
-    String getAlamat() {
-        return this.Alamat;
+
+    String getNamaResto() {
+        return namaResto;
     }
-    String getNoTelp() {
-        return this.noTelp;
+
+    void setNamaResto(String namaResto) {
+        this.namaResto = namaResto;
     }
-    String getNmk1(){return this.nmk1;}
-    String getNmk2(){return this.nmk2;}
-    String getNmk3(){return this.nmk3;}
-    float getHm1(){return this.hm1;}
-    float getHm2(){return this.hm2;}
-    float getHm3(){return this.hm3;}
-    void setNama(String Nama) {
-        this.Nama = Nama;
+
+    String getAlamatResto() {
+        return alamatResto;
     }
-    void setRating(Float Rating) {
-        this.Rating = Rating;
+
+    void setAlamatResto(String alamatResto) {
+        this.alamatResto = alamatResto;
     }
-    void setAlamat(String Alamat) {
-        this.Alamat=Alamat;
+
+    ArrayList<Utama> getMakananResto() {
+        return makananResto;
     }
-    void setNoTelp(String noTelp){
-        this.noTelp=noTelp;
+
+    void setMakananResto(ArrayList<Utama> makananResto) {
+        this.makananResto = makananResto;
     }
-    void setNmk1(String nmk1){ this.nmk1 =nmk1;}
-    void setNmk2(String nmk2){ this.nmk2 =nmk2;}
-    void setNmk3(String nmk3){ this.nmk2 =nmk3;}
-    void setNmk3(Float hm3){ this.hm3 =hm3;}
-    void setNmk2(Float hm2){ this.hm2 =hm2;}
-    void setNmk1(Float hm1){ this.hm1 =hm1;}
-    public String toString(){
-        String result = "";
-        result = "\nNama Restoran : " + Nama + "\nAlamat Restoran : " + Alamat + "\nNomor Telepon Restoran : " + noTelp + "\nNama Makanan/Minuman : " + nmk1 + "\nHarga Makanan/Minuman : " + hm1 + "\nNama Makanan/Minuman : " + nmk2 + "\nHarga Makanan/Minuman : " + hm2 + "\nNama Makanan/Minuman : " + nmk3 + "\nHarga Makanan/Minuman : " +  hm3;
-        return result;
+
+    void addMakananResto(Utama makananResto) {
+        this.makananResto.add(makananResto);
+    }
+
+    ArrayList<Utama> getMinumanResto() {
+        return minumanResto;
+    }
+
+    void setMinumanResto(ArrayList<Utama> minumanResto) {
+        this.minumanResto = minumanResto;
+    }
+
+    void addMinumanResto(Utama minumanResto) {
+        this.minumanResto.add(minumanResto);
     }
 }
-
 
 class Kelola {
 
